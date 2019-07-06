20a1ec2ce4b1ce62175c015583eb7365de0643be
Olawumi Yusuff <qauzeemyusuff2@gmail.com>
2019-07-06 14:46:14 +0100

    lab3(task 6): set qauzeemyusuff2scrumy/urls index path

--- myscrumy/myscrumy/urls.py
+++ myscrumy/myscrumy/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('qauzeemyusuff2scrumy/', include('qauzeemyusuff2scrumy.urls'))
+    path('qauzeemyusuff2scrumy/', include('qauzeemyusuff2scrumy.urls')),
 ]
