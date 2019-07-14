3dfa71e5cd10ed9ac3f9705e8e57c261cb09feb4
Olawumi Yusuff <qauzeemyusuff2@gmail.com>
2019-07-14 11:56:14 +0100

    lab9: create models in app

--- myscrumy/qauzeemyusuff2scrumy/models.py
+++ myscrumy/qauzeemyusuff2scrumy/models.py
@@ -1,13 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
+class GoalStatus(models.Model):
+    status_name = models.CharField(max_length=100)
 
 class ScrumyGoals(models.Model):
-
+    goal_name = models.CharField(max_length=100)
+    goal_id = models.IntegerField(primary_key=True)
+    created_by = models.CharField(max_length=100)
+    moved_by = models.CharField(max_length=100)
+    owner = models.CharField(max_length=100)
+    goal_status = models.ForeignKey(GoalStatus, on_delete=models.PROTECT)
+    user = models.ForeignKey(User, models.CASCADE, related_name='goals')
 
 class ScrumyHistory(models.Model):
-
-
-class GoalStatus(models.Model):
-
+  moved_by = models.CharField(max_length=100)
+  created_by = models.CharField(max_length=100)
+  moved_from = models.CharField(max_length=100)
+  moved_to = models.CharField(max_length=100)
+  time_of_action = models.DateTimeField(auto_now_add=True)
+  goal = models.ForeignKey(ScrumyGoals, on_delete=models.CASCADE)
